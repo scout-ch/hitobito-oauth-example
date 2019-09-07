ccef369f7c95a8953c0f2a213771db6f04394f44
Diego Steiner <diego.steiner@u041.ch>
2019-09-07 08:17:46 +0200

    Fix apis

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ApplicationController
   respond_to :json
 
   def explore
-    @json = doorkeeper_access_token.get("api/v1/#{params[:api]}").parsed
+    @json = doorkeeper_access_token.get(params[:api]).parsed
     respond_with @json
   end
 end
