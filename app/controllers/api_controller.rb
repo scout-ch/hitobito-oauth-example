dcb796711ad2f12e3f030c3d8cd3bea2ac465dee
Diego Steiner <diego.steiner@u041.ch>
2019-09-07 08:30:47 +0200

    Add scope

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ApplicationController
   respond_to :json
 
   def explore
-    @json = doorkeeper_access_token.get(params[:api]).parsed
+    @json = doorkeeper_access_token.get(params[:api], headers: { 'X-Scope' => 'with_roles' }).parsed
     respond_with @json
   end
 end
